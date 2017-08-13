6986610194546153919380fe120853e00f4f2a3d
chenziyuncs <76058789@qq.com>
2017-08-13 20:18:39 +0800

    项目架构

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import header from '../components/header/header.vue'
+import goods from '../components/goods/goods.vue'
+import ratings from '../components/ratings/ratings.vue'
+import seller from '../components/seller/seller.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path:'/',
+      redirect:'/goods'
+    },
+    {
+      path: '/goods',
+      component: goods
+    },
+    {
+      path: '/seller',
+      component: seller
+    },
+    {
+      path: '/ratings',
+      component: ratings
     }
   ]
 })
