c4b9bf9f7a04651e08830c27e0d8da3a1f5a4280
chenziyuncs <76058789@qq.com>
2017-08-15 00:03:53 +0800

    完成头部功能

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,7 +13,7 @@
           蜂鸟专送/38分钟送达
         </div>
         <div class="supports">
-          <span class="icon special"></span>
+          <span class="icon decrease"></span>
           <span class="text">在线支付满28减5，满50减10</span>
           </div>
       </div>
@@ -29,7 +29,65 @@
       </span>
       <span class="icon-keyboard_arrow_right"></span>
     </div>
-    
+    <div class="background">
+      <img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" width="100%" height="100%" />
+    </div>
+    <div class="mask">
+      <div class="mask-wrapper">
+        <div class="mask-main">
+          <h1 class="title">粥品香坊(大运村)</h1>
+          <div class="stars-wrapper">
+            <div class="starts stars-24">
+              <span class="star on"></span>
+              <span class="star on"></span>
+              <span class="star on"></span>
+              <span class="star half"></span>
+              <span class="star off"></span>
+            </div>
+          </div>
+          <div class="info">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul class="list">
+            <li>
+              <span class="icon decrease"></span>
+              <span class="text">在线支付满28减2个隔壁老王</span>
+            </li>
+            <li>
+              <span class="icon decrease"></span>
+              <span class="text">vc无限果汁全场8折只限老司机</span>
+            </li>
+            <li>
+              <span class="icon decrease"></span>
+              <span class="text">双人原谅绿套餐</span>
+            </li>
+            <li>
+              <span class="icon decrease"></span>
+              <span class="text">该商家支持发票，请下单写好发票</span>
+            </li>
+            <li>
+              <span class="icon decrease"></span>
+              <span class="text">已加入“隔壁老王培养”计划，食品安全保障</span>
+            </li>
+          </ul>
+          <div class="info">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="content">
+            <p class="text">
+              粥品香坊其烹饪粥料的秘方来源于中国千年古法，在融合现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然，0添加的良心品质深得消费者青睐，发展至今称为粥类引领品牌。是2008年奥运会和2013年园博会指定餐饮服务商。
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="mask-footer">
+        <span class="icon-close"></span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,6 +97,7 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
 @import "../../common/stylus/mixin.styl"
+
   .header
     position relative
     color rgb(255,255,255)
@@ -118,7 +177,7 @@
         line-height 12px
         background-color rgba(0,0,0,0.2)
         span
-          line-height 24px
+          line-height 24px 
     .bulletin-wrapper
       font-size 10px
       height 28px
@@ -144,4 +203,119 @@
         right 12px
         line-height 28px
         bottom 0
+    .background
+      position absolute
+      left 0
+      top 0
+      background-color rgba(7,17,27,0.2)
+      width 100%
+      height 100%
+      z-index -1
+      filter blur(10px)
+    .mask
+      position fixed
+      left 0
+      top 0
+      font-size 22px
+      width 100%
+      height 100%
+      background-color rgba(7,17,27,.8)
+      z-index 99
+      overflow auto
+      .mask-wrapper
+        min-height 100%
+        .mask-main
+          padding-top 64px
+          padding-bottom 64px
+          .title
+            font-size 16px
+            line-height 16px
+            font-weight 700
+            text-align center
+          .stars-wrapper
+            height 24px
+            margin 16px 0 28px  0
+            .starts
+              text-align center
+              .star
+                display inline-block
+                background-repeat no-repeat
+                background-size 100%  100%
+              &.stars-24
+                .star
+                  width 20px
+                  height 19px
+                  &.on
+                    bg-image("../stars/star36_on")
+                  &.half
+                    bg-image("../stars/star36_half")
+                  &.off
+                    bg-image("../stars/star36_off")
+
+          .info
+            width 80%
+            display flex
+            margin 0  auto
+            .line
+              flex 1
+              background-color rgba(255,255,255,.2)
+              height 1px
+              position relative
+              top 8px
+            .text
+              margin 0  12px
+              font-size 16px
+              line-height 16px
+              font-weight 700
+              text-align center
+          .list
+            width 80%
+            margin-top 24px
+            margin 0  auto  24px  28px
+            li
+              margin-bottom 12px
+              &:last-child
+                margin-bottom 0
+              .icon
+                display  inline-block
+                width 16px
+                height 16px
+                background-repeat no-repeat
+                background-size 100%  100%
+                &.decrease
+                  bg-image(decrease_2)
+                &.discount
+                  bg-image(discount_2)
+                &.guarantee
+                  bg-image(guarantee_2)
+                &.invoice
+                  bg-image(invoice_2)
+                &.special
+                  bg-image(special_2)
+              .text 
+                font-size 12px
+                line-height 12px
+                font-weight 200
+                color rgb(255,255,255)
+          .content
+            .text
+              font-size 12px
+              font-weight 200
+              color rgb(255,255,255)
+              line-height 24px
+              margin  24px  36px  0 36px
+      .mask-footer
+        text-align center
+        margin-top -64px
+        font-size 32px
+        color rgba(255,255,255,.5)
+
+
+
+
+
+
+
+
+
 </style>
